60648930ad376263dc7af4c10fa3c1647523d205
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2024-01-03 15:56:39 +0100

    ss

--- utils/db.ts
+++ utils/db.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
   }
   prisma = global.prisma;
 }
+
+export default prisma;
